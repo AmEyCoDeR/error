ff6572cab9743b2aaa60256087acbb74f16b2791
AmEyCoDeR <99733292+AmEyCoDeR@users.noreply.github.com>
2022-04-30 12:36:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var errorstate = "";
+
+function setup() {
+
+
+
+}
+
+function draw() {
+    background("white");
+    if(errorstate === ""){
+        openoo()
+    }
+  
+}
+
+function openoo(){
+ window.open("https://ameycoder.github.io/error")
+}
